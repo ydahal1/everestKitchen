251c44a5b0cef9d3304bd1b412756be4d321df54
Yadhap Dahal <yadhapdahal111@gmail.com>
2018-08-16 11:10:22 -0400

    added files

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//Inject all the pages to index.html
+$(document).ready(function(){
+    $("#homePageDiv").load("assets/pages/home.html");
+});
+
+
---
+++ assets/javascript/info.js
@@ -0,0 +1,499 @@
+var menu = {
+    soupsAndSalad : [
+                    ["Soups and Salads",
+                     "Mutton Soup",
+                     3.50,
+                     "Mutton softly flavored with aromatic herbs and spices"
+                    ],
+                    ["Soups and Salads",
+                     "Chicken Soup",
+                     3.25,
+                     "Chicken softly flavored with aromatic herbs and spices"
+                    ],
+                    ["Soups and Salads",
+                     "Pulse (Daal Soup)",
+                     2.99,
+                     "Soup Prepared with herbs and spices in traditional Bhutanese style"
+                    ],
+                    ["Soups and Salads",
+                     "Nepali Salad",
+                     4.99,
+                     "Slices of fresh carrot, cucumber, tomato, lime and red onions"
+                    ]
+                    ],
+    appetizers : [
+                ["Appetizers",
+                "Vegetable Samosa(2 pcs)",
+                3.49,
+                "Crispy pastry stuffed with potatoes, green peas, and deep fried"
+                ],
+                ["Appetizers",
+                "Onion (Pakora) bhaji(6 pcs)",
+                5.49,
+                "Sliced onions mixed with mild spices, chick peas flour and deep fried"
+                ],
+                ["Appetizers",
+                "Chicken Pakora (6 pcs)",
+                5.99,
+                "Deep fried chicken in mild spices"
+                ],
+                ["Appetizers",
+                "Aloo Pakora (6 pcs)",
+                4.99,
+                "Deep fried potato in mild spices"
+                ],
+                ["Appetizers",
+                "Vegetable platter",
+                10.99,
+                "Assortment of aloo pokora, samosa, vegetable pokora and more"
+                ],
+                ["Appetizers",
+                "Vegetable spring roll",
+                5.99,
+                "A blend of fresh cabbage, carrots, and onions with a hint of seasoning, wrapped in a deliciously flaky and crispy spring wrap"
+                ],
+                ["Appetizers",
+                "Samosa chat",
+                5.99,
+                "Samosa pieces topped with mint sauce, tamarind sauce, yogurt, beans & spices"
+                ],
+                ["Appetizers",
+                "Aloo dum zira",
+                6.50,
+                "Fried potatoes cooked in spice, rich yogurt gravy, red chili, coriander, and cumin seeds"
+                ],
+                ["Appetizers",
+                "Vatmas acchar sadeko",
+                6.99,
+                "Roasted soybeans mixed with spices, onions, green chilly chopped, ginger, garlic, and lemon juice"
+                ],
+                ["Appetizers",
+                "Vatmas acchar sadeko",
+                6.99,
+                "Roasted soybeans mixed with spices, onions, green chilly chopped, ginger, garlic, and lemon juice"
+                ]
+            ],
+    momo: [
+        ["MO:MO Varieties",
+        "Steamed veg MO:MO (10 pcs)",
+        7.50,
+        "Dish-steamed dumplings stuffed with mixed vegetables and served with special (dipping) sauce"
+        ],
+        ["MO:MO Varieties",
+        "Fried veg MO:MO (10 pcs)",
+        7.75,
+        "Pan-fried dumplings served with special (dipping) sauce"
+    ],
+        ["MO:MO Varieties",
+        "Steamed chicken MO:MO (10 pcs)",
+        8.25,
+        "Dish-steamed dumplings stuffed with ground chicken and served with special (dipping) sauce"
+    ],
+        ["MO:MO Varieties",
+        "Fried chicken MO:MO (10 pcs)",
+        8.50,
+        "Pan-fried dumplings served with special (dipping) sauce"
+        ], 
+        ["MO:MO Varieties",
+        "Steamed beef MO:MO (10 pcs)",
+        8.50,
+        "Dish-steamed dumplings stuffed with ground beef and served with special (dipping) sauce"
+        ],  
+        ["MO:MO Varieties",
+        "Fried beef MO:MO (10 pcs)",
+        8.75,
+        "Pan-fried dumplings served with special (dipping) sauce"
+        ],  
+        ["MO:MO Varieties",
+        "Steamed pork MO:MO (10 pcs)",
+        7.99,
+        "Dish-steamed dumplings stuffed with ground pork and served with special (dipping) sauce"
+        ], 
+        ["MO:MO Varieties",
+        "Fried pork MO:MO (10 pcs)",
+        8.24,
+        "Pan-fried dumplings served with special (dipping) sauce"
+        ], 
+        ["MO:MO Varieties",
+        "C (Chili) MO:MO - Chicken / Veg /Pork (10 pcs)",
+        10.99,
+        "MO: MO in spicy tomato sauce"
+        ], 
+        ["MO:MO Varieties",
+        "Jhol MO:MO - Chicken / Veg /Pork (10 pcs)",
+        8.50,
+        "MO: MO (stuffed dumplings) with cold sesame, garlic, tomato and onion broth"
+        ]
+    ],
+chowMein:[
+        ["Chow Mein Varieties",
+        "Veg chow mein",
+        7.49,
+        "Spaghetti soaked in hot water and fried with vegetables in the frying pan"
+        ],
+        ["Chowmein Varieties",
+        "Chicken Chow mein",
+        7.99,
+        "Spaghetti soaked in hot water and fried with chicken in the frying pan"
+        ],
+        ["Chowmein Varieties",
+        "Mutton Chow mein",
+        8.99,
+        "Spaghetti soaked in hot water and fried with mutton in the frying pan"
+        ],
+        ["Chowmein Varieties",
+        "Pork Chow mein",
+        8.99,
+        "Spaghetti soaked in hot water and fried with pork in the frying pan"
+        ]
+    ],
+    chowChow :  [
+            ["Chow Chow / Noodles Varieties",
+            "Chicken Chow Chow",
+            6.75,
+            "Nepali style noodles pan-fried with vegetables, chicken and spices"
+            ],
+            ["Chow Chow / Noodles Varieties",
+            "Veg Chow Chow",
+            6.50,
+            "Nepali style noodles pan-fried with vegetables and spices"
+            ],
+            ["Chow Chow / Noodles Varieties",
+            "Mutton Chow Chow",
+            6.99,
+            "Nepali style noodles pan-fried with vegetables, mutton and spices"
+            ]
+        ],
+
+    thukpa : [
+            ["Thukpa Varieties",
+            "VEG THUKPA",
+            6.50,
+            "Typical Nepali style boiled noodles with vegetables and seasoning"
+            ],
+            ["Thukpa Varieties",
+            "CHICKEN THUKPA ",
+            7.50,
+            "Nepali style boiled noodle soup mixed with cilantro, cabbage, carrot, and spinach"
+            ],
+            ["Thukpa Varieties",
+            "MUTTON THUKPA",
+            8.50,
+            "Nepali style boiled noodle soup mixed with cilantro, cabbage, carrot, and spinach"
+            ],
+            ["Thukpa Varieties",
+            "PORK THUKPA",
+            8.25,
+            "Nepali style boiled noodle soup mixed with cilantro, cabbage, carrot, and spinach"
+            ]
+            ],
+    friedRice : [
+            ["Fried Rice Varieties",
+            "VEG FRIED RICE",
+            8.25,
+            "Stir-fried basmati rice on chopped onion, bell pepper, green peas, and toasted cumin seeds.Garnished with cilantro."
+            ],
+            ["Fried Rice Varieties",
+            "CHICKEN FRIED RICE",
+            8.50,
+            "Stir-fried basmati rice on chopped onion, bell pepper, green peas, and toasted cumin seeds.Garnished with cilantro"
+            ],
+            ["Fried Rice Varieties",
+            "MUTTON FRIED RICE",
+            8.99,
+            "Stir-fried basmati rice on chopped onion, bell pepper, green peas, and toasted cumin seeds.Garnished with cilantro"
+            ],
+            ["Fried Rice Varieties",
+            "PORK FRIED RICE",
+            8.75,
+            "Stir-fried basmati rice on chopped onion, bell pepper, green peas, and toasted cumin seeds.Garnished with cilantro"
+            ],
+            ["Fried Rice Varieties",
+            "SHRIMP FRIED RICE",
+            9.99,
+            "Stir-fried basmati rice on chopped onion, bell pepper, green peas, and toasted cumin seeds.Garnished with cilantro"
+            ]
+        ],
+    riceAndCurry : [
+                ["Rice with veg and non veg varieties",
+                "MUTTOR PANEER w/ RICE",
+                10.99,
+                "Homemade cheese cooked with fresh green peas in a light creamy sauce with herbs and spices"
+                ],
+                ["Rice with veg and non veg varieties",
+                "SPINACH PANEER w/ RICE",
+                10.99,
+                "Paneer and spinach cooked with fresh onions, tomatoes, and cream"
+                ],
+                ["Rice with veg and non veg varieties",
+                "CHICKEN SPINACH w/ RICE",
+                10.99,
+                "Chicken and spinach cooked with fresh onions, tomatoes, and cream"
+                ],
+                ["Rice with veg and non veg varieties",
+                "CHICKEN CURRY w/ RICE ON SIDE",
+                10.99,
+                "Chicken cooked in a typical Nepali village style with local herbs and spices"
+                ],
+                ["Rice with veg and non veg varieties",
+                "LAMB CURRY w/ RICE",
+                10.99,
+                "Lamb cooked in a typical Nepali village style with local herbs and spices"
+                ],
+                ["Rice with veg and non veg varieties",
+                "MUTTON CURRY w/ RICE ON SIDE",
+                11.15,
+                "Mutton cooked in a typical Nepali village style with local herbs and spices"
+                ],
+                ["Rice with veg and non veg varieties",
+                "PORK CURRY w/ RICE",
+                10.99,
+                "Pork cooked in a typical Nepali village style with local herbs and spices"
+                ],
+                ["Rice with veg and non veg varieties",
+                "FISH CURRY w/ RICE",
+                12.99,
+                "Fish cooked in a typical Nepali village style with local herbs and spices"
+                ],
+                ["Rice with veg and non veg varieties",
+                "CHICKEN TIKKA MASALA w/RICE",
+                10.99,
+                "Tender chicken meat cooked with special mild spicy tomato sauce & local herbs and served with rice"
+                ]
+            ],
+    sukuti : [
+            ["Sukuti (Dried Meat Varities)",
+            "MUTTON SADEKO SUKUTI (DRIED MUTTON)",
+            13.99,
+            "Dried mutton deep fried with tomatoes, onions, herbs, spices, chili, & served with pickles of roasted soya beans"
+            ],
+            ["Sukuti (Dried Meat Varities)",
+            "PORK SADEKO SUKUTI (DRIED PORK) ",
+            11.25,
+            "Dried pork deep fried with tomatoes, onions, herbs, spices, chili, & served with pickles of roasted soya beans"
+            ],
+            ["Sukuti (Dried Meat Varities)",
+            "CHICKEN SADEKO SUKUTI (DRIED CHICKEN)",
+            10.99,
+            "Dried chicken deep fried with tomatoes, onions, herbs, spices, chili, & served with pickles of roasted soya beans"
+            ]
+        ],
+    sekuwa : [
+            ["Sekuwa",
+            "PORK SEKUWA",
+            12.99,
+            "Nepali style grilled pork mixed with special sauce, onions, chili, natural herbs, and spices"
+            ]
+        ],
+
+    seaFood :[
+            ["Sea Food",
+            "GARLIC BUTTER SHRIMP",
+            11.99,
+            "Shrimp roasted in lots of butter & chopped garlic, parsley, salt & pepper, and served with a lemon wedge"
+            ],
+            ["Sea Food",
+            "BONE-IN FRIED FISH (4 PCS) ",
+            10.99,
+            "Deep fried fish"
+            ],
+            ["Sea Food",
+            "BONELESS FRIED FISH (4 PCS)",
+            11.99,
+            "Small pieces of Fish, delicately spiced and batter fried"
+            ],
+            ["Sea Food",
+            "HOT & SPICY SHRIMP",
+            11.99,
+            "Shrimp roasted in chopped garlic with half of a lemon, spices, parsley, salt & hot pepper/chilies, and served with a lemon wedge"
+            ]
+        ],
+    everestThaali : [
+            [
+            "Everest Thaali Set (Dinner Size)",
+            "VEG THAALI",
+            12.99,
+            "Combination platter served with basmati rice, veg curry, pickles, pulse soup, papad, and yogurt"
+            ],
+            [
+            "Everest Thaali Set (Dinner Size)",
+            "CHICKEN THAALI",
+            14.99,
+            "Combination platter served with chicken, basmati rice, veg curry, pickles, pulse soup, papad, & yogurt"
+            ],
+            [
+            "Everest Thaali Set (Dinner Size)",
+            "MUTTON THAALI",
+            15.99,
+            "Combination platter served with mutton, basmati rice, pulse soup, pickles, veg curry, papad & yogurt"
+            ],
+            [
+            "Everest Thaali Set (Dinner Size)",
+            "FISH THAALI",
+            14.99,
+            "Combination platter served with fish, basmati rice, pulse soup, veg curry, pickles, papad & yogurt"
+            ],
+            [
+            "Everest Thaali Set (Dinner Size)",
+            "LAMB THAALI",
+            14.99,
+            "Combination platter served with lamb, basmati rice, pulse soup, veg curry, pickles, papad & yogurt"
+            ],
+            [
+            "Everest Thaali Set (Dinner Size)",
+            "PORK THAALI ",
+            14.99,
+            "Combination platter served with pork, basmati rice, pulse soup, veg curry, pickles, papad & yogurt"
+            ]       
+        ],
+    bhutaneseDishes : [
+            [ "BHUTANESE TYPICAL DISHES (Served only on weekends)",
+            "BHUTANESE DISHES SET",
+            13.99,
+            "Combination platter served with red rice, Ema Datshi, Kewa Datshi, Phaksha Paa, Goen Hogay,Ezay, & Suja"
+            ],
+            [ "BHUTANESE TYPICAL DISHES (Served only on weekends)",
+            "EMA DATSHI (Chilies & Cheese)",
+            "Included in Bhutanese Dishes Set",
+            "Sliced fresh green chilies or dry red chilies cooked with datshi & plenty of butter for good measure"
+            ],  
+            [ "BHUTANESE TYPICAL DISHES (Served only on weekends)",
+            "KEWA DATSHI (Potato & Cheese)",
+            "Included in Bhutanese Dishes Set",
+            "Typical thin sliced of potatoes, then sautéed down with cheese, lots of butter, & chilies or tomatoes"
+            ],
+            [ "BHUTANESE TYPICAL DISHES (Served only on weekends)",
+            "PHAKSHA PAA (Pork with Radish or Green Mustard Leaves)",
+            "Included in Bhutanese Dishes Set",
+            "Slices of pork are stir fried with whole red dry chilies, & sliced radish or green mustard leaves"
+            ],
+            [ "BHUTANESE TYPICAL DISHES (Served only on weekends)",
+            "GOEN HOGAY (Bhutanese Cucumber Salad)",
+            "Included in Bhutanese Dishes Set",
+            "Sliced cucumber mixed with chili flakes, tomatoes, cilantro, onions, Sichuan pepper, & a crumble of cheese for extra flavor"
+            ],
+            [ "BHUTANESE TYPICAL DISHES (Served only on weekends)",
+            "EZAY (Bhutanese Chili Sauces)",
+            "Included in Bhutanese Dishes Set",
+            "Mixture of chilies, Sichuan pepper, & cheese. Tree tomatoes or regular tomatoes can also be added."
+            ],
+            [ "BHUTANESE TYPICAL DISHES (Served only on weekends)",
+            "PHAKSHA PAA w/WHITE RICE",
+            11.49,
+            "" 
+            ] 
+        ],
+    kidsMeal :[
+        [ "Kids Meal",
+        "FRENCH FRIES ",
+        2.99,
+        "" 
+        ],
+        [ "Kids Meal",
+        "CHICKEN TENDERS (4 PCS) WITH FRENCH FRIES",
+        4.99,
+        "" 
+        ],
+        [ "Kids Meal",
+        "CHICKEN NUGGETS (6 PCS) WITH FRENCH FRIES",
+        5.99,
+        "" 
+        ],
+        [ "Kids Meal",
+        "CHICKEN TENDERS (4 PCS) ",
+        2.99,
+        "" 
+        ],
+        [ "Kids Meal",
+        "CHICKEN NUGGETS (6 PCS)",
+        3.00,
+        "" 
+        ],
+        [ "Kids Meal",
+        "BOWL OF WHITE RICE",
+        1.99,
+        "" 
+        ]
+        ],
+    desserts : [
+        [ "Desserts",
+        "MANGO ICE CREAM",
+        1.99,
+        "" 
+        ],
+        [ "Desserts",
+        "GULAB JAMUN",
+        1.99,
+        "Soft fried cheese balls soaked in honey syrup" 
+        ],
+        [ "Desserts",
+        "KHEER",
+        2.50,
+        "A flavorful rice pudding cooked with milk, cardamom, raisins, cashews, and almonds" 
+        ]
+        ],
+    hotBeverages : [
+        [ "Hot Beverages",
+        "MILK TEA ",
+        2.50,
+        "" 
+        ],
+        [ "Hot Beverages",
+        "RED TEA ",
+        2.25,
+        "" 
+        ],
+        [ "Hot Beverages",
+        "BLACK COFFEE",
+        2.99,
+        "" 
+        ]
+        ],
+    coldBeverages : [
+        [ "Cold Beverages",
+        " SOFT DRINKS (12 OZ) ",
+        1.25,
+        "Coke/Diet Coke/Iced Tea/Sprite/Mt. Dew/Pepsi/Fanta" 
+        ],
+        [ "Cold Beverages",
+        "BOTTLED COKE (20 OZ)",
+        2.25,
+        "" 
+        ],
+        [ "Cold Beverages",
+        "BOTTLED FANTA (20 OZ)",
+        2.25,
+        "" 
+        ],
+        [ "Cold Beverages",
+        "MANGO JUICE ",
+        1.95,
+        "" 
+        ],
+        [ "Cold Beverages",
+        "MANGO LASSI",
+        2.50,
+        "" 
+        ],
+        [ "Cold Beverages",
+        "THIN YOGURT DRINKS (REGULAR OR SWEET)",
+        2.50,
+        "" 
+        ],
+        [ "Cold Beverages",
+        "GLASS COKE/FANTA/SPRITIE",
+        2.00,
+        "" 
+        ],
+        [ "Cold Beverages",
+        "MILKSHAKE",
+        2.50,
+        "Homemade shake from milk sweetened in Nepali styles" 
+        ],
+        [ "Cold Beverages",
+        "ENERGY DRINKS (MONSTER/RED BULL)",
+        2.50,
+        "Homemade shake from milk sweetened in Nepali styles" 
+        ]
+    ]
+}
