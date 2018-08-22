7564865b681ed6ea746853b822b9e36affb28b47
Yadhap Dahal <yadhapdahal111@gmail.com>
2018-08-22 17:30:13 -0400

    fixed styleing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,27 @@
-//Inject all the pages to index.html
-$(document).ready(function(){
-    $("#homePageDiv").load("assets/pages/home.html");
-});
+// Business Opening and closing hours
+var today = new Date();
+var day = today.getDay();
+var time = today.getHours();
+var year = today.getFullYear();
 
 
+function businessStatus(){
+if (day == 2 || time > 23 || time < 11 ){
+    console.log("closed");
+    $("#businessStatus").html("CLOSED NOW");
+    $("#statusDot").addClass("redDot");
+    console.log("its closed");
+}else{
+    $("#businessStatus").html("CURRENTLY OPEN");
+    $("#statusDot").addClass("greenDot");
+    console.log("its open");
+}
+}
+
+setInterval( function(){
+    businessStatus();
+}, 2000);
+
+
+//CopyRight 
+$("#currentYear").html(year);
